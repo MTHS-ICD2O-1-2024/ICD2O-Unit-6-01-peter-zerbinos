4581f8cb9d47b45c1609ac94d2beff2dfc381f8a
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-13 16:39:15 +0000

    added code from Unit-5-03

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Peter Zerbinos All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+function ageCheck() {
+  const userAge = parseInt(document.getElementById('user-age').value)
+
+  if (userAge >= 17) {
+    document.getElementById('result').innerHTML =
+      '<p>You can go see an R rated movie alone.</p>'
+  }
+  
+    else if (userAge >= 13) {
+    document.getElementById('result').innerHTML =
+      '<p>You can go see a PG-13 movie alone.</p>'
+  }
+
+    else if (userAge >= 5) {
+    document.getElementById('result').innerHTML =
+      '<p>You can go see a G or PG movie alone.</p>'
+  }
+
+  else {
+    document.getElementById('result').innerHTML =
+      '<p>You are too young to see most things.</p>'
+  }
+}
